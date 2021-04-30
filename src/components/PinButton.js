d3ff527a7bb638b868583679e8a4e7f6ffa2c365
Jake Gray <jakegray1717@gmail.com>
2021-04-30 15:47:53 -0700

    score strikes in tenth frame significant progress

--- src/components/PinButton.js
+++ src/components/PinButton.js
@@ -5,6 +5,9 @@ const PinButton = ({ buttonValue, setCurrentRollIdx, currentRollIdx, setFrames, 
   const enterPins = (e) => {
     let pins = buttonValue
     const rollOne = frames[currentFrameIdx].frame[0];
+
+
+
     if (buttonValue === 10 && currentRollIdx === 0) {
       pins = 'X';
       setFrames(frames => {
@@ -12,7 +15,11 @@ const PinButton = ({ buttonValue, setCurrentRollIdx, currentRollIdx, setFrames, 
         newFrames[currentFrameIdx].frame[currentRollIdx] = pins;
         return [...newFrames];
       });
-      setCurrentFrameIdx(currentFrameIdx + 1);
+      if (currentFrameIdx !== 9) {
+        setCurrentFrameIdx(currentFrameIdx + 1);
+      } else {
+        setCurrentRollIdx(currentRollIdx + 1)
+      }
     } else if (buttonValue + rollOne === 10 && currentRollIdx === 1) {
       pins = '/';
       setFrames(frames => {
@@ -31,7 +38,13 @@ const PinButton = ({ buttonValue, setCurrentRollIdx, currentRollIdx, setFrames, 
     }
   }
 
-  if (10 - frames[currentFrameIdx].frame[0] >= buttonValue || currentRollIdx === 0 || currentRollIdx === 2) {
+  if (currentFrameIdx === 9 && currentRollIdx === 1) {
+    return (
+      <>
+        <Button variant="secondary" onClick={(e) => enterPins(e)}>{buttonValue}</Button>{' '}
+      </>
+    )
+  } else if (10 - frames[currentFrameIdx].frame[0] >= buttonValue || currentRollIdx === 0 || currentRollIdx === 2) {
     return (
       <>
         <Button variant="secondary" onClick={(e) => enterPins(e)}>{buttonValue}</Button>{' '}
